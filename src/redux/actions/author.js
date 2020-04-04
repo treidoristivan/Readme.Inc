b2c1124b580c90677a6b4d8b51f610933c90d330
agusrichard <agus.richard21@gmail.com>
2020-04-04 22:58:00 +0700

    Redux author

--- src/redux/actions/author.js
+++ src/redux/actions/author.js
@@ -3,6 +3,6 @@ import {APP_URL, Get} from '../../config/config';
 export const getAuthor = () => {
     return {
         type: 'GET_AUTHOR',
-        payload: Get(APP_URL.concat('/author'))
+        payload: Get(APP_URL.concat('/authors'))
     }
 }
