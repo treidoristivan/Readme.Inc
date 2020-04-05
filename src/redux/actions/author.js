7062d6c703c47b5c00b80fdc17f540293f5c3032
agusrichard <agus.richard21@gmail.com>
2020-04-05 16:52:09 +0700

    Infinite scrolling

--- src/redux/actions/author.js
+++ src/redux/actions/author.js
@@ -1,8 +1,8 @@
 import {APP_URL, Get} from '../../config/config';
 
-export const getAuthor = () => {
+export const getAuthor = (pageNumber) => {
     return {
         type: 'GET_AUTHOR',
-        payload: Get(APP_URL.concat('/authors'))
+        payload: Get(APP_URL.concat('/authors?page=' + pageNumber))
     }
 }
--- src/screens/Author.js
+++ src/screens/Author.js
@@ -1,11 +1,13 @@
 //import liraries
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, ScrollView, Image, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, Image, TouchableOpacity, FlatList, ActivityIndicator } from 'react-native';
 import SliderTitle from '../components/SliderTitle';
 import { connect } from 'react-redux';
 import { getAuthor } from '../redux/actions/author';
 import { withNavigation } from 'react-navigation';
+import { List } from 'native-base'
 import { APP_IMAGE_URL } from '../config/config';
+
 
 // create a component
 class AuthorOriginal extends Component {
@@ -14,61 +16,94 @@ class AuthorOriginal extends Component {
         this.state = {
             isLoading: true,
             isSuccess: false,
-
+            data: [],
+            page: 1
         }
     }
 
     async componentDidMount() {
-        await this.props.dispatch(getAuthor())
+        await this.props.dispatch(getAuthor(this.state.page))
+        this.setState({
+            data: this.props.author.data
+        })
         await this.setState({
             isLoading: false,
         })
     }
 
+    fetchData = async () => {
+        this.setState(prevState => ({ isLoading: true }))
+        await this.props.dispatch(getAuthor(this.state.page))
+        this.setState(prevState => ({ 
+            data: [...prevState.data, ...this.props.author.data],
+            isLoading: false
+        }))
+    }
+
+    handleEnd = () => {
+        console.log('handleEnd')
+        this.setState(prevState => ({ page: prevState.page + 1 }), () => this.fetchData())
+    }
+
     render() {
+        console.log('AUTHOR DATA', this.props.author.data)
         return (
             <View style={styles.container}>
               <View style={styles.headerWrapper}>
                     <SliderTitle title="Author" />
                 </View>
-                <ScrollView
-                    vertikal
-                    showsVerticalScrollIndicator={true}
-                >
-                    {this.state.isLoading &&
-                        <>
-                            <View style={[styles.card, { marginHorizontal: 30 }]}>
-                                <View style={styles.cardWrapper}>
-                                    <View style={{ backgroundColor: '#eee', width: 250, height: 50 }}></View>
-                                    <View style={{ backgroundColor: '#eee', height: 10, width: 250, marginTop: 5 }}></View>
+                <List style={{ paddingBottom: 60 }}>
+                    <FlatList
+                        data={this.state.data}
+                        keyExtractor={(item) => item.id}
+                        onEndReached={() => this.handleEnd()}
+                        onEndReachedThreshold={0.5}
+                        renderItem={({ item }) => {
+                            var img = <Image source={{ uri: item.author_image }} style={{ width: 100, height: 100, borderRadius: 70, }} resizeMode="cover" />
+                            var styled = [styles.card]
+                            // if (i === 0) {
+                            //     styled.push({ marginHorizontal: 30 })
+                            // }
+                            // if (i === this.props.author.data.length - 1) {
+                            //     styled.push({ marginRight: 20 })
+                            // }
+                            return (
+                                <>
+                                    <TouchableOpacity style={styled} key={item.id} onPress={() => this.props.navigation.navigate('AuthorDetail', { authorId: item.id })}>
+                                    <View style={styles.card}>
+                                        <View style={styles.cardWrapper}>
+                                            {img}
+                                            <Text style={styles.titles}>Author Name :</Text>
+                                            <Text style={styles.title}>{item.author_name}</Text>
+                                        </View>
+                                        </View>
+                                    </TouchableOpacity>
+                                </>
+                            )
+                        }}
+                    />
+                        {/* {this.state.isLoading &&
+                            <>
+                                <View style={[styles.card, { marginHorizontal: 30 }]}>
+                                    <View style={styles.cardWrapper}>
+                                        <View style={{ backgroundColor: '#eee', width: 250, height: 50 }}></View>
+                                        <View style={{ backgroundColor: '#eee', height: 10, width: 250, marginTop: 5 }}></View>
+                                    </View>
                                 </View>
-                            </View>
+                                
+                            </>
+                        } */}
+                        {/* {!this.state.isLoading && this.props.author.data.map((v, i) => {
                             
-                        </>
-                    }
-                    {!this.state.isLoading && this.props.author.data.map((v, i) => {
-                        var img = <Image source={{ uri: v.author_image }} style={{ width: 100, height: 100, borderRadius: 70, }} resizeMode="cover" />
-                        var styled = [styles.card]
-                        if (i === 0) {
-                            styled.push({ marginHorizontal: 30 })
-                        }
-                        if (i === this.props.author.data.length - 1) {
-                            styled.push({ marginRight: 20 })
-                        }
-                        return (
-                            <TouchableOpacity style={styled} key={i} onPress={() => this.props.navigation.navigate('AuthorDetail', { authorId: v.id })}>
-                            <View style={styles.card}>
-                                <View style={styles.cardWrapper}>
-                                    {img}
-                                    <Text style={styles.titles}>Author Name :</Text>
-                                    <Text style={styles.title}>{v.author_name}</Text>
-                                </View>
-                                </View>
-                            </TouchableOpacity>
-                        )
-                    })}
-                </ScrollView>
-            </View >
+                        })} */}
+                    <ActivityIndicator 
+                        animating={this.state.isLoading} 
+                        color="blue"
+                        size="large"
+                    />
+                </List>
+                
+            </View>
         );
     }
 }
